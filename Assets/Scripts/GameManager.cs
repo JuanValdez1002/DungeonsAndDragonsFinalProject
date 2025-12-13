9b2d65dff276a90883c2e27b09a52c838e85be8f
Jannette Carranza <jannettecarranza@gmail.com>
2025-12-12 20:54:13 -0600

    ?

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    public GameObject player;
+
+    public void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (gameOverUI.activeInHierarchy)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void gameOver()
+        {
+            gameOverUI.SetActive(true);
+        }
+
+    public void RestartLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Debug.Log("Restarting Level...");
+        }
+
+    public void MainMenu()
+        {
+            SceneManager.LoadScene("Game_Menu");
+            Debug.Log("Loading Main Menu...");
+        }
+
+    public void QuitGame()
+        {
+            Application.Quit();
+            Debug.Log("Quitting Game...");
+
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+
+    
+}
+
